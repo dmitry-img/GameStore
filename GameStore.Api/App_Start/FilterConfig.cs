98cdd12cffab1dff1dfc066754956ddc5b4f481d
Дмитрий Паскар <seller_firegram@outlook.com>
2023-04-03 20:55:57 +0300

    GMSTR-1: added NotFoundException filter

--- GameStore.Api/App_Start/FilterConfig.cs
+++ GameStore.Api/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using GameStore.Api.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace GameStore.Api
@@ -8,6 +9,8 @@ namespace GameStore.Api
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new NotFoundExceptionFilter());
+            filters.Add(new LogIpFilter());
         }
     }
 }
---
+++ GameStore.Api/Filters/LogIpFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.Api.Filters
+{
+    public class LogIpFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //TODO finish the LogIp filter
+            string appDataPath = HttpContext.Current.Server.MapPath("~/App_Data");
+            string path = Path.Combine(appDataPath, "RequestantIps.txt");
+
+            var ip = filterContext.HttpContext.Request;
+            File.WriteAllText(path, $"Date: {DateTime.UtcNow} - Ip: {ip}");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            
+        }
+    }
+}
